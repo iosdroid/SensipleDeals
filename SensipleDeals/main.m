b1acf8bc325b2caa1b0327316ab0bdc143bcef02
Muthupalani <muthupalaniappan@sensiples-mac-mini.local>
2016-06-09 17:45:38 +0530

    Initial Commit

---
+++ SensipleDeals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SensipleDeals
+//
+//  Created by Muthupalaniappan S on 09/06/16.
+//  Copyright © 2016 sensiple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
